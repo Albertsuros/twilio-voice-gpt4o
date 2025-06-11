3ad2e47abf17758063447f96656f76576a958b72
Albertsuros <albesur@gmail.com>
2025-06-11 22:43:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -134,8 +134,8 @@ async function synthesizeWithElevenLabs(text, req) {
         text: text.trim(),
         model_id: "eleven_multilingual_v2",
         voice_settings: {
-          stability: 0.2,
-          similarity_boost: 1.0
+          stability: 0.4,
+          similarity_boost: 0.75
         }
       },
       {
